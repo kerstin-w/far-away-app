f1ede1532677b321c9297b6b5dcacd5526509d14
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-04-14 19:19:38 +0200

    feat: Create select options in Form component. The component renders a dropdown menu (select element) with 20 options. Each option corresponds to a number from 1 to 20.The Array.from method is used to create a new array instance from an object. The map function is then used to transform this array of numbers into an array of option elements for the dropdown menu.

--- src/App.js
+++ src/App.js
@@ -25,9 +25,18 @@ function Logo() {
 
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
