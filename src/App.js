9758774f92eba67a034b64c86a1293f8fa8a51fd
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-04-14 18:59:08 +0200

    feat: Create the PackingList items. A component for the list item is created which is containing the quantity and  * description of the item, with a strikethrough style applied if the item is marked as packed. The The PackingList component is mapping each list item

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 /**
  * The App function is a React component that renders a logo, a form, a packing list, and stats within
  * a div with the class name "app".
@@ -26,7 +32,32 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+/**
+ * The Item function renders a list item with a quantity, description, and a button in JavaScript.
+ * @returns The `Item` function is returning a list item (`<li>`) element containing the quantity and
+ * description of the item, with a strikethrough style applied if the item is marked as packed.
+ * Additionally, there is a button with a cross mark emoji inside the list item.
+ */
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
